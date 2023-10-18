77ca0a2477598230f0e1d37ba78a04152b463c6b
aniilab <alilinalinaa@icloud.com>
2023-10-18 20:33:45 +0300

    DbContext modelBuilder entity Event added

--- WelcomeHome/WelcomeHome.DAL/WelcomeHomeDbContext.cs
+++ WelcomeHome/WelcomeHome.DAL/WelcomeHomeDbContext.cs
@@ -20,6 +20,22 @@ public sealed class WelcomeHomeDbContext : DbContext
 			        .HasIndex(u => u.Email)
 			        .IsUnique();
 
+        modelBuilder.Entity<Event>().HasIndex(e => e.Id)
+                            .IsUnique();
 
+        modelBuilder.Entity<Event>().HasOne(e => e.Volunteer)
+                                    .WithMany(v => v.Events)
+                                    .HasForeignKey(e => e.VolunteerId)
+                                    .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Event>().HasOne(e => e.EventType)
+                                    .WithMany(et => et.Events)
+                                    .HasForeignKey(e => e.EventTypeId)
+                                    .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Event>().HasOne(e => e.Establishment)
+                                    .WithMany(es => es.Events)
+                                    .HasForeignKey(e => e.EstablishmentId)
+                                    .OnDelete(DeleteBehavior.Cascade);
     }
 }
