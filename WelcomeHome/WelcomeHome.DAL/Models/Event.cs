e428f803160142755599599bc20b6b8c34c47870
aniilab <alilinalinaa@icloud.com>
2023-10-18 20:18:54 +0300

    Event model changed (VolunteerId added)

--- WelcomeHome/WelcomeHome.DAL/Models/Event.cs
+++ WelcomeHome/WelcomeHome.DAL/Models/Event.cs
@@ -11,6 +11,7 @@ namespace WelcomeHome.DAL.Models
         public string Description { get; set; } = null!;
         public int EstablishmentId { get; set; }
         public int EventTypeId { get; set; }
+        public int VolunteerId { get; set; }
         public Establishment? Establishment { get; set; }
         public EventType? EventType { get; set; }
         public Volunteer Volunteer { get; set; } = null!;
