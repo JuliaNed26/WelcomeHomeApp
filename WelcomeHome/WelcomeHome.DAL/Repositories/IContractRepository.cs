2056455f4215d1ffb4ac26b56bc2a04fc93170e5
@yuliia_nedavnia <yuliia_nedavnia@epam.com>
2023-10-21 10:30:44 +0300

    added_contract_repository_interface

---
+++ WelcomeHome/WelcomeHome.DAL/Repositories/IContractRepository.cs
@@ -0,0 +1,16 @@
+﻿using WelcomeHome.DAL.Models;
+
+namespace WelcomeHome.DAL.Repositories;
+
+public interface IContractRepository
+{
+	Task<Contract> GetByIdAsync(int id);
+
+	IEnumerable<Contract> GetAll();
+
+	Task AddAsync(Contract contract);
+
+	Task UpdateAsync(Contract contract);
+
+	Task DeleteAsync(int id);
+}
