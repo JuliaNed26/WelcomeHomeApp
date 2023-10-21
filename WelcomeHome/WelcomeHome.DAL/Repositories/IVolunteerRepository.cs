42f1f04beb9f044d28c01295b52e6f251a0ff410
@yuliia_nedavnia <yuliia_nedavnia@epam.com>
2023-10-21 10:52:51 +0300

    added_volunteer_repository_interface

---
+++ WelcomeHome/WelcomeHome.DAL/Repositories/IVolunteerRepository.cs
@@ -0,0 +1,16 @@
+﻿using WelcomeHome.DAL.Models;
+
+namespace WelcomeHome.DAL.Repositories;
+
+public interface IVolunteerRepository
+{
+	Task<Volunteer> GetByIdAsync(int id);
+
+	IEnumerable<Volunteer> GetAll();
+
+	Task AddAsync(Volunteer volunteer);
+
+	Task UpdateAsync(Volunteer volunteer);
+
+	Task DeleteAsync(int id);
+}
