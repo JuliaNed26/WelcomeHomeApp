b8fe27e75e479ce1596433942c2a8b573f49c352
@yuliia_nedavnia <yuliia_nedavnia@epam.com>
2023-10-21 10:32:15 +0300

    changed_contracts_model

--- WelcomeHome/WelcomeHome.DAL/Models/Contracts.cs
+++ WelcomeHome/WelcomeHome.DAL/Models/Contracts.cs
@@ -8,10 +8,10 @@ namespace WelcomeHome.DAL.Models
     {
         [Key]
         public int Id { get; set; }
+        public int VolunteerId { get; set; }
         public Volunteer Volunteer { get; set; } = null!;
         public DateTime DateStart { get; set; }
         public DateTime DateEnd { get; set; }
         public string URL { get; set; }
-
     }
 }
