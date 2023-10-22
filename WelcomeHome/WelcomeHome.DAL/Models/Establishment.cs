07cfeb129d7a1639de4385ec1b4617f108b4c77d
aniilab <alilinalinaa@icloud.com>
2023-10-23 00:56:33 +0300

    Establishment repository added

--- WelcomeHome/WelcomeHome.DAL/Models/Establishment.cs
+++ WelcomeHome/WelcomeHome.DAL/Models/Establishment.cs
@@ -10,11 +10,12 @@ namespace WelcomeHome.DAL.Models
         public Guid Id { get; set; }
         public string Name { get; set; } = null!;
         public string Address { get; set; } = null!;
-        public City City { get; set; } = null!;
         public int EstablishmentTypeId { get; set; }
+        public int CityId { get; set; }
         public string? PageURL { get; set; }
         public string? PhoneNumber { get; set; }
         public string? OtherContacts { get; set; }
+        public City City { get; set; } = null!;
         public EstablishmentType EstablishmentType { get; set; } = null!;
         public ICollection<Event>? Events { get; set; }
 
