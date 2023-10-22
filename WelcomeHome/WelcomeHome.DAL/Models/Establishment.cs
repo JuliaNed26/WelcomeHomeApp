aec971697465170730ad33f1ed91125ee26daaf4
aniilab <alilinalinaa@icloud.com>
2023-10-23 00:39:09 +0300

    Establishments model changed and added to context

--- WelcomeHome/WelcomeHome.DAL/Models/Establishment.cs
+++ WelcomeHome/WelcomeHome.DAL/Models/Establishment.cs
@@ -11,10 +11,11 @@ namespace WelcomeHome.DAL.Models
         public string Name { get; set; } = null!;
         public string Address { get; set; } = null!;
         public City City { get; set; } = null!;
-        public EstablishmentType EstablishmentType { get; set; } = null!;
+        public int EstablishmentTypeId { get; set; }
         public string? PageURL { get; set; }
         public string? PhoneNumber { get; set; }
         public string? OtherContacts { get; set; }
+        public EstablishmentType EstablishmentType { get; set; } = null!;
         public ICollection<Event>? Events { get; set; }
 
     }
