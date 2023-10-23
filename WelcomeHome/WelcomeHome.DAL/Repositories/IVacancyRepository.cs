2478bf94f7eb030334238db131bcbbae05291fae
Yulia Solomakha <yulichka700@gmail.com>
2023-10-23 22:07:47 +0200

    Vacancy Repository

---
+++ WelcomeHome/WelcomeHome.DAL/Repositories/IVacancyRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WelcomeHome.DAL.Models;
+
+namespace WelcomeHome.DAL.Repositories
+{
+    public interface IVacancyRepository
+    {
+        Task<Vacancy?> GetByIdAsync(Guid id);
+
+        IEnumerable<Vacancy> GetAll();
+
+        Task AddAsync(Vacancy vacancy);
+
+        Task UpdateAsync(Vacancy vacancy);
+
+        Task DeleteAsync(Guid id);
+    }
+}
---
+++ WelcomeHome/WelcomeHome.DAL/Repositories/VacancyRepository.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WelcomeHome.DAL.Models;
+
+namespace WelcomeHome.DAL.Repositories
+{
+    public sealed class VacancyRepository : IVacancyRepository
+    {
+        private readonly WelcomeHomeDbContext _context;
+        
+        public VacancyRepository(WelcomeHomeDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Vacancy vacancy)
+        {
+            await _context.AddAsync(vacancy).ConfigureAwait(false);
+            await _context.SaveChangesAsync().ConfigureAwait(false);
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var foundVacancy = await _context.Vacancies.SingleAsync(v => v.Id == id).ConfigureAwait(false);
+            _context.Vacancies.Remove(foundVacancy);
+            await _context.SaveChangesAsync().ConfigureAwait(false);
+        }
+
+        public IEnumerable<Vacancy> GetAll()
+        {
+            return _context.Vacancies
+                .Include(v => v.Establishment)
+                .AsNoTracking()
+                .Select(v => v);
+        }
+
+        public async Task<Vacancy?> GetByIdAsync(Guid id)
+        {
+            return await _context.Vacancies
+                .Include(v => v.Establishment)
+                .AsNoTracking()
+                .SingleOrDefaultAsync(v => v.Id == id)
+                .ConfigureAwait(false);
+        }
+
+        public async Task UpdateAsync(Vacancy vacancy)
+        {
+            var foundVacancy = await _context.Vacancies
+                                         .SingleAsync(v => v.Id == vacancy.Id)
+                                         .ConfigureAwait(false);
+
+            foundVacancy.Name = vacancy.Name;
+            foundVacancy.Salary = vacancy.Salary;
+            foundVacancy.Description = vacancy.Description;
+            foundVacancy.PageURL = vacancy.PageURL;
+            foundVacancy.PhoneNumber = vacancy.PhoneNumber;
+            foundVacancy.OtherContacts = vacancy.OtherContacts;
+            //foundVacancy.Establishment = vacancy.Establishment;
+
+            _context.Vacancies.Update(foundVacancy);
+
+            await AttachEstablishmentAsync(vacancy).ConfigureAwait(false);
+
+            await _context.SaveChangesAsync().ConfigureAwait(false);
+        }
+
+        private async Task AttachEstablishmentAsync(Vacancy vacancy)
+        {
+            var foundEstablishment = await _context.Establishments
+                                         .SingleAsync(e => e.Id == vacancy.EstablishmentId)
+                                         .ConfigureAwait(false);
+
+            _context.Establishments.Attach(foundEstablishment);
+            _context.Entry(foundEstablishment).State = EntityState.Unchanged;
+        }
+    }
+}
