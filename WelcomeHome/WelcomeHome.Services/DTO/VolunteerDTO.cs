8355d8704913fdb15ea6d1c9975bc2e5a3b81ad3
Yulia Solomakha <yulichka700@gmail.com>
2023-11-05 23:15:51 +0100

    Folders for Services and DTOs

---
+++ WelcomeHome/WelcomeHome.Services/DTO/VolunteerDTO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeHome.Services.DTO
+{
+    public class VolunteerDTO
+    {
+        public string FullName { get; set; } = null!;
+
+        public string PhoneNumber { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string Telegram { get; set; }
+
+        public string? Document { get; set; }
+
+        public string? EstablishmentId { get; set; }
+
+        public string? EstablishmentName { get; set; }
+
+        public string? EstablishmentCityName { get; set; }
+
+        public string? EstablishmentPageURL { get; set; }
+
+        public string? EstablishmentPhoneNumber { get; set; }
+
+        public string? EstablishmentOtherContacts { get; set; }
+
+    }
+}
