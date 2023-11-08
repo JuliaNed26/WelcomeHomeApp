0d1289188a44d192597dc82c44bdc8e3ff070a8d
aniilab <alilinalinaa@icloud.com>
2023-11-08 22:58:57 +0200

    Document DTOs added

---
+++ WelcomeHome/WelcomeHome.Services/DTO/DocumentOutDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WelcomeHome.DAL.Models;
+
+namespace WelcomeHome.Services.DTO
+{
+    public class DocumentOutDTO
+    {
+        public string Name { get; set; }
+        public ICollection<StepDocument> StepDocuments { get; set; }
+    }
+}
