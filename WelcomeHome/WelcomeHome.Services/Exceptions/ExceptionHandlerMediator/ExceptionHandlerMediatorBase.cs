dc65000f7e41e32de44f959000cc4d92899f5ce1
JuliaNedavnia <nedavnaia@knu.ua>
2023-11-10 10:22:42 +0200

    Exception handlers and mediators added

---
+++ WelcomeHome/WelcomeHome.Services/Exceptions/ExceptionHandlerMediator/ExceptionHandlerMediatorBase.cs
@@ -0,0 +1,40 @@
+﻿using WelcomeHome.Services.Exceptions.ExceptionHandlers;
+
+namespace WelcomeHome.Services.Exceptions.ExceptionHandlerMediator;
+
+public abstract class ExceptionHandlerMediatorBase
+{
+	protected DataExceptionHandler HandlersChain { get; init; }
+
+	public async Task HandleAndThrowAsync(Func<Task> action)
+	{
+		try
+		{
+			await action().ConfigureAwait(false);
+		}
+		catch (Exception ex)
+		{
+			HandleException(ex);
+			throw;
+		}
+	}
+
+	public async Task<T> HandleAndThrowAsync<T>(Func<Task<T>> action)
+	{
+		try
+		{
+			var result = await action().ConfigureAwait(false);
+			return result;
+		}
+		catch (Exception ex)
+		{
+			HandleException(ex);
+			throw;
+		}
+	}
+
+	protected void HandleException(Exception ex)
+	{
+		HandlersChain.Handle(ex);
+	}
+}
