8c3d62a13cfe68338a60753c87a916e048dd44cd
Yulia Solomakha <yulichka700@gmail.com>
2023-11-11 16:22:11 +0100

    AuthService added + volunteer- and user- add methods changed

--- WelcomeHome/WelcomeHome.Services/Services/VolunteerService.cs
+++ WelcomeHome/WelcomeHome.Services/Services/VolunteerService.cs
@@ -12,20 +12,44 @@ namespace WelcomeHome.Services.Services
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
         private readonly ExceptionHandlerMediatorBase _exceptionHandler;
+        private readonly IAuthService _authService;
 
-		public VolunteerService(IUnitOfWork unitOfWork, IMapper mapper, ExceptionHandlerMediatorBase exceptionHandler)
+		public VolunteerService(IUnitOfWork unitOfWork, IMapper mapper, ExceptionHandlerMediatorBase exceptionHandler, IAuthService authService)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
             _exceptionHandler = exceptionHandler;
+            _authService = authService;
         }
 
         public async Task AddAsync(VolunteerInDTO newVolunteer)
         {
+            /*
 	        await _exceptionHandler.HandleAndThrowAsync(() => _unitOfWork
 			                                                  .VolunteerRepository
 			                                                  .AddAsync(_mapper.Map<Volunteer>(newVolunteer)))
 		        .ConfigureAwait(false);
+            */
+
+            //I didn't figure out how to combine mapper and password shifr, so I wrote my code here 
+            byte[] hash;
+            byte[] salt;
+            _authService.CreatePasswordHash(newVolunteer.Password,out hash, out salt);
+
+            Volunteer volunteer = new Volunteer
+            {
+                Id = new Guid(),
+                PasswordHash = hash,
+                PasswordSalt = salt,
+                FullName = newVolunteer.FullName,
+                PhoneNumber = newVolunteer.PhoneNumber,
+                Email = newVolunteer.Email,
+                Telegram = newVolunteer.Telegram,
+                Document = newVolunteer.Document,
+                EstablishmentId = newVolunteer.EstablishmentId
+            };
+
+            await _exceptionHandler.HandleAndThrowAsync(()=>_unitOfWork.VolunteerRepository.AddAsync(volunteer));
         }
 
         public async Task DeleteAsync(Guid id)
@@ -58,6 +82,7 @@ namespace WelcomeHome.Services.Services
 
         public async Task UpdateAsync(VolunteerOutDTO volunteerWithUpdateInfo)
         {
+            //should we update the pswrd?
 	        await _exceptionHandler.HandleAndThrowAsync(() => _unitOfWork
 			                                                  .VolunteerRepository
 			                                                  .UpdateAsync(_mapper.Map<Volunteer>(volunteerWithUpdateInfo)))
