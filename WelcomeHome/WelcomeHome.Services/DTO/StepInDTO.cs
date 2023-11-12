a0ed6ba7ff328a543c7104c789ded94903b0bf8b
aniilab <alilinalinaa@icloud.com>
2023-11-12 23:52:17 +0200

    Step service added

---
+++ WelcomeHome/WelcomeHome.Services/DTO/StepInDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WelcomeHome.DAL.Models;
+
+namespace WelcomeHome.Services.DTO
+{
+    public record StepInDTO
+    {
+        public string Description { get; set; }
+
+        public Guid EstablishmentTypeId { get; set; }
+
+    }
+}
