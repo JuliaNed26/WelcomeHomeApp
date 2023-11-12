aec566d49dee83f4703053bd367ed12b06706689
aniilab <alilinalinaa@icloud.com>
2023-11-13 00:07:29 +0200

    Added ReferenceHandler = ReferenceHandler.Preserve to Program

--- WelcomeHome/WelcomeHome.Web/Program.cs
+++ WelcomeHome/WelcomeHome.Web/Program.cs
@@ -1,10 +1,15 @@
+using System.Text.Json.Serialization;
 using WelcomeHome.Web;
 using WelcomeHome.Web.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+            });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
