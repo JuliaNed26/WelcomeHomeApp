06e430946bacc520d8ab6ceb2ca72683b7577599
JuliaNedavnia <nedavnaia@knu.ua>
2023-11-12 08:30:55 +0200

    Added controllers for city and country

---
+++ WelcomeHome/WelcomeHome.Web/Controllers/CityController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WelcomeHome.Services.DTO;
+using WelcomeHome.Services.Services;
+
+namespace WelcomeHome.Web.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class CityController : ControllerBase
+{
+	private readonly ICityCountryService _cityCountryService;
+
+	public CityController(ICityCountryService cityCountryService)
+	{
+		_cityCountryService = cityCountryService;
+	}
+
+	[HttpGet]
+	public ActionResult<IEnumerable<CityOutDto>> GetAll()
+	{
+		var allCities = _cityCountryService.GetAllCities();
+		return Ok(allCities);
+	}
+
+	[HttpGet("country/{countryId}")]
+	public ActionResult<IEnumerable<CityOutDto>> GetForCountry(Guid countryId)
+	{
+		var cities = _cityCountryService.GetAllCitiesForCountry(countryId);
+		return Ok(cities);
+	}
+}
