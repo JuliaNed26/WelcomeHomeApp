f0987cdbad359d36a56b77bcd2d0e8cbc372e67f
@yuliia_nedavnia <yuliia_nedavnia@epam.com>
2024-02-04 21:42:50 +0200

    Psychological tsenter added to establishment types

---
+++ WelcomeHome/WelcomeHome.DAL/Migrations/20240204193846_PsychologicalTsenterToEstablishmentType.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WelcomeHome.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class PsychologicalTsenterToEstablishmentType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                INSERT INTO dbo.EstablishmentTypes(Name) VALUES (N'Психологічний центр');
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
