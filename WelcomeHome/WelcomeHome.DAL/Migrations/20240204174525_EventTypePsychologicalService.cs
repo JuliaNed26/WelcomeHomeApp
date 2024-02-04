c31e136da9bfe4978ba36bb6265ae00fd6603116
@yuliia_nedavnia <yuliia_nedavnia@epam.com>
2024-02-04 19:51:39 +0200

    Social service added to event types

---
+++ WelcomeHome/WelcomeHome.DAL/Migrations/20240204174525_EventTypePsychologicalService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WelcomeHome.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class EventTypePsychologicalService : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                INSERT INTO EventTypes (Name)
+                VALUES (N'Психологічний сервіс');
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
