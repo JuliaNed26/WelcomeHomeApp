cdb51ba393a7456f320bd66252530c617f514375
@yuliia_nedavnia <yuliia_nedavnia@epam.com>
2024-02-05 14:05:46 +0200

    Added base service fixture

---
+++ WelcomeHome/WelcomeHome.Services.Tests/Services/BaseServiceFixture.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using NSubstitute;
+using WelcomeHome.DAL.UnitOfWork;
+
+namespace WelcomeHome.Services.Tests.Services;
+
+[TestFixture]
+public class BaseServiceFixture
+{
+    public IUnitOfWork UnitOfWork { get; private set; }
+    public IMapper Mapper { get; private set; }
+
+    [OneTimeSetUp]
+    public void InitializeMapper()
+    {
+        var mapperConfig = new MapperConfiguration(cfg =>
+        {
+            cfg.AddMaps(typeof(AutoMapperProfile).Assembly);
+        });
+        Mapper = mapperConfig.CreateMapper();
+    }
+
+    [SetUp]
+    public void InitializeUnitOfWork()
+    {
+        UnitOfWork = Substitute.For<IUnitOfWork>();
+    }
+}
