8c7c7f4fb86ebcc5a97f78f8f21a9d3ec90c9075
@yuliia_nedavnia <yuliia_nedavnia@epam.com>
2024-02-11 17:00:57 +0200

    Volunteer model changed

---
+++ WelcomeHome/WelcomeHome.DAL/Migrations/20240211141129_VolunteerUpdated.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WelcomeHome.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class VolunteerUpdated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Establishments_Volunteers_CreatorId",
+                table: "Establishments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Volunteers_Establishments_OrganizationId",
+                table: "Volunteers");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "OrganizationId",
+                table: "Volunteers",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVerified",
+                table: "Volunteers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Establishments_Volunteers_CreatorId",
+                table: "Establishments",
+                column: "CreatorId",
+                principalTable: "Volunteers",
+                principalColumn: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Volunteers_Establishments_OrganizationId",
+                table: "Volunteers",
+                column: "OrganizationId",
+                principalTable: "Establishments",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Establishments_Volunteers_CreatorId",
+                table: "Establishments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Volunteers_Establishments_OrganizationId",
+                table: "Volunteers");
+
+            migrationBuilder.DropColumn(
+                name: "IsVerified",
+                table: "Volunteers");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "OrganizationId",
+                table: "Volunteers",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Establishments_Volunteers_CreatorId",
+                table: "Establishments",
+                column: "CreatorId",
+                principalTable: "Volunteers",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Volunteers_Establishments_OrganizationId",
+                table: "Volunteers",
+                column: "OrganizationId",
+                principalTable: "Establishments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- WelcomeHome/WelcomeHome.DAL/Models/Volunteer.cs
+++ WelcomeHome/WelcomeHome.DAL/Models/Volunteer.cs
@@ -8,8 +8,9 @@ namespace WelcomeHome.DAL.Models
         public long UserId { get; set; }
         public User User { get; set; }
         public string SocialUrl { get; set; }
-        public long OrganizationId { get; set; }
-        public Establishment Organization { get; set; }
+        public bool IsVerified { get; set; }
+        public long? OrganizationId { get; set; }
+        public Establishment? Organization { get; set; }
         public ICollection<Event>? Events { get; set; }
         public ICollection<Establishment>? Establishments { get; set; }
     }
