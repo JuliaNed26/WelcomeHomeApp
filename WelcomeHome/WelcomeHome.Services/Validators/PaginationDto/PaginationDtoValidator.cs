6ca122af040c55ab57dc15a531a9784be9c7d1b2
@yuliia_nedavnia <yuliia_nedavnia@epam.com>
2024-03-02 12:55:24 +0200

    Pagination dto validator added

---
+++ WelcomeHome/WelcomeHome.Services/Validators/PaginationDto/PaginationDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using WelcomeHome.Services.DTO.VacancyDTO;
+
+namespace WelcomeHome.Services.Validators.PaginationDto;
+
+internal class PaginationDtoValidator : AbstractValidator<PaginationOptionsDTO>
+{
+    public PaginationDtoValidator()
+    {
+        RuleFor(p => p.CountOnPage).GreaterThan(0);
+        RuleFor(p => p.PageNumber).GreaterThan(0);
+    }
+}
