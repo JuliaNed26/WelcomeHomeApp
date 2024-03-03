4b20ca8d6c65a91dc1e2c48d495902e45db36ecf
@yuliia_nedavnia <yuliia_nedavnia@epam.com>
2024-03-03 12:29:03 +0200

    Vacancies with total pages count procedure added to migrations

---
+++ WelcomeHome/WelcomeHome.DAL/Migrations/20240303102705_VacanciesWithTotalPagesCountProcedureAdded.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WelcomeHome.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class VacanciesWithTotalPagesCountProcedureAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE PROCEDURE GetVacancyPageWithTotalVacanciesCount
+                                    	@page int = 1,
+                                    	@countOnPage int = 20
+                                    AS
+                                    BEGIN
+                                    	SELECT ROUND((COUNT(*) OVER ()) / @countOnPage, 0) AS TotalPagesCount, *
+                                    	FROM Vacancies
+                                    	ORDER BY Id
+                                    	OFFSET (@page - 1) * @countOnPage ROWS
+                                    	FETCH NEXT @countOnPage ROWS ONLY;
+                                    END");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
