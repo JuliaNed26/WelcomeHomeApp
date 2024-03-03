0535427c22eb816f355628add42a92f5de774462
@yuliia_nedavnia <yuliia_nedavnia@epam.com>
2024-03-03 13:52:19 +0200

    Stored procedure to retrieve page from database changed

--- WelcomeHome/WelcomeHome.DAL/Migrations/20240303102705_VacanciesWithTotalPagesCountProcedureAdded.cs
+++ WelcomeHome/WelcomeHome.DAL/Migrations/20240303102705_VacanciesWithTotalPagesCountProcedureAdded.cs
@@ -11,16 +11,27 @@ namespace WelcomeHome.DAL.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql(@"CREATE PROCEDURE GetVacancyPageWithTotalVacanciesCount
-                                    	@page int = 1,
-                                    	@countOnPage int = 20
-                                    AS
-                                    BEGIN
-                                    	SELECT ROUND((COUNT(*) OVER ()) / @countOnPage, 0) AS TotalPagesCount, *
-                                    	FROM Vacancies
-                                    	ORDER BY Id
-                                    	OFFSET (@page - 1) * @countOnPage ROWS
-                                    	FETCH NEXT @countOnPage ROWS ONLY;
-                                    END");
+                                   	    @page int = 1,
+										@countOnPage int = 20
+									AS
+									BEGIN
+										SELECT ROUND((COUNT(*) OVER ()) / @countOnPage, 0) AS TotalPagesCount,
+										       Vacancies.Id,
+											   Vacancies.Name,
+											   Vacancies.CompanyName,
+											   Vacancies.Description, 
+											   Vacancies.Salary,
+											   Vacancies.PageURL,
+											   Vacancies.PhoneNumber,
+											   Vacancies.OtherContacts,
+											   Vacancies.CityId,
+											   Cities.Name As CityName
+										FROM Vacancies
+										     LEFT JOIN Cities ON Vacancies.CityId = Cities.Id
+										ORDER BY Vacancies.Id
+										OFFSET (@page - 1) * @countOnPage ROWS
+										FETCH NEXT @countOnPage ROWS ONLY;
+									END");
         }
 
         /// <inheritdoc />
